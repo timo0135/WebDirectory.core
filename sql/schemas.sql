8f1f0573958532ce8691d38ae818d4ca9b2707e0
despaqui9u <liamdespaqiis@gmail.com>
2024-06-12 16:08:22 +0200

    scriptSQL

---
+++ sql/schemas.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : mysql_container
+-- Généré le : mer. 12 juin 2024 à 09:32
+-- Version du serveur : 8.0.37
+-- Version de PHP : 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `WebDirectory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `entree`
+--
+
+CREATE TABLE `entree` (
+  `id` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nom` varchar(128) NOT NULL, 
+  `prenom` varchar(128) NOT NULL, 
+  `fonction` varchar(128) NOT NULL, 
+  `numeroBureau` int NOT NULL,
+  `numeroTel1` varchar(10) NOT NULL,
+  `numeroTel2` varchar(10) DEFAULT NULL, 
+  `email` varchar(10) NOT NULL,
+  `image` varchar(128) DEFAULT 'person.png',
+  `statut` int NOT NULL DEFAULT '1',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `entree2Departement`
+--
+
+CREATE TABLE `entree2Departement` (
+  `entree_id` varchar(128) NOT NULL,
+  `departement_id` varchar(128) NOT NULL,
+  `quantite` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `departement`
+--
+
+CREATE TABLE `departement` (
+  `id` int NOT NULL,
+  `nom` varchar(128) NOT NULL,
+  `etage` varchar(5) NOT NULL,
+  `description` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `role` tinyint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+--
+-- Index pour la table `entree`
+--
+ALTER TABLE `entree`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `departement`
+--
+ALTER TABLE `departement`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_pk2` (`user_id`);
+
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `departement`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
