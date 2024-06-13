700d565cf1705ae3c175116f42ed1cd2a50a09e5
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2024-06-13 16:33:58 +0200

    Contrôle d’accès

--- admin/src/app/actions/GetCreateEntreeAction.php
+++ admin/src/app/actions/GetCreateEntreeAction.php
@@ -5,22 +5,29 @@ namespace webDirectory\admin\app\actions;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use webDirectory\admin\app\providers\auth\AuthProviderInterface;
+use webDirectory\admin\app\providers\auth\SessionAuthProvider;
 use webDirectory\admin\app\utils\CsrfService;
 
 class GetCreateEntreeAction extends Action
 {
     private string $template;
+    private AuthProviderInterface $authProvider;
 
     public function __construct()
     {
         $this->template = 'entreesCreate.html.twig';
+        $this->authProvider = new SessionAuthProvider();
     }
 
     public function __invoke(Request $rq, Response $rs, $args): Response
     {
-        $view  = Twig::fromRequest($rq);
-        return $view->render($rs, $this->template, [
-            'csrf'=> CsrfService::generate()
-        ]);
+        if ($this->authProvider->isSignedIn()){
+            $view  = Twig::fromRequest($rq);
+            return $view->render($rs, $this->template, [
+                'csrf'=> CsrfService::generate()
+            ]);
+        }
+        return $rs->withHeader('Location', '/signin')->withStatus(302);
     }
 }
